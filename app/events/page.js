982a6cc676ad5eb5587f0285a74a1941710da18d
Santam Kumai <santtamk@gmail.com>
2024-11-07 12:17:21 +0530

    fixed Missing Suspense boundary with useSearchParams

--- app/events/page.js
+++ app/events/page.js
@@ -1,13 +1,15 @@
-
-
-
+import { Suspense } from "react";
 import EventsList from "./EventsList";
 
 const EventsPage = async () => {
   const res = await fetch("https://qevent-backend.labs.crio.do/events");
   const events = await res.json();
 
-  return <EventsList events={events} />;
+  return (
+    <Suspense>
+      <EventsList events={events} />
+    </Suspense>
+  );
 };
 
 export default EventsPage;
